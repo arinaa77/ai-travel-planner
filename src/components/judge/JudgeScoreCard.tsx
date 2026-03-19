2ea51976dd718c0178ba544a2088eaf745ab6bcf
Yihan Wang <arinayih@gmail.com>
2026-03-19 16:27:11 -0700

    feat(judge): implement JudgeScoreCard

---
+++ src/components/judge/JudgeScoreCard.tsx
@@ -0,0 +1,24 @@
+import { JudgeEvaluation } from "@/lib/mockData";
+
+interface Props {
+  evaluation: JudgeEvaluation;
+}
+
+export default function JudgeScoreCard({ evaluation }: Props) {
+  return (
+    <div>
+      <div>{evaluation.overallScore}</div>
+
+      {evaluation.scores.map((s) => (
+        <div key={s.dimension}>
+          <span>{s.dimension}</span>
+          <span>{s.score}</span>
+          <div
+            data-testid="score-bar"
+            style={{ width: `${s.score}%` }}
+          />
+        </div>
+      ))}
+    </div>
+  );
+}
