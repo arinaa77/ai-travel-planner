dedf125697cb0a3bee5cef7505d414a6ddda30d6
Yihan Wang <arinayih@gmail.com>
2026-04-18 14:26:57 -0700

    chore(lint): add Prettier with eslint-config-prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,7 +11,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   { ignores: [".next/**", "out/**", "build/**", "node_modules/**", "next-env.d.ts"] },
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
 ];
 
 export default eslintConfig;
